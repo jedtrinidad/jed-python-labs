8f74371c15af0f929891de5495c8071d5fa1c83c
Jed Cyril Trinidad <ubuntu@ip-172-31-20-82.ap-southeast-1.compute.internal>
2020-03-06 02:03:34 +0000

    Completed d1e1_csv_parser.py

--- d1e1_csv_parser.py
+++ d1e1_csv_parser.py
@@ -3,11 +3,33 @@ import csv
 def get_rows(file):
     rows = []
     with open(file) as csvfile:
-        csvreader = csv.DictReader(csvfile)
+        print("Reading File...")
+        csvdialect = csv.Sniffer().sniff(csvfile.read(1024), delimiters=',')
+        csvfile.seek(0)
+        csvreader = csv.DictReader(csvfile, dialect=csvdialect)
         for row in csvreader:
             rows.append(row)
     return rows
     
-filename = 'sample_products.csv'
+def remove_items_without_categories(items):
+    clean_items = []
+    print("Removing Items without categories.")
+    for item in items:
+        if item['Categories'] is '':
+            items.remove(item)
+    clean_items = items
+    return clean_items
+            
+def write_items(items):
+    print("Writing cleaned items to file: 'clean_sample_products.csv'")
+    with open('clean_sample_products.csv', 'w', newline='') as csvfile:
+        fields = list(items[0].keys())
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        
+        writer.writeheader()
+        writer.writerows(items)
 
+filename = 'sample_products.csv'
 items = get_rows(filename)
+cleaned_items = remove_items_without_categories(items)
+write_items(cleaned_items)
